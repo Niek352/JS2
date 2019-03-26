8f3f5e2b378ba159c93178696a212dd49cf9fa1d
Niek352 <oavelasdqwe8@gmail.com>
2019-03-26 23:10:26 +0300

    hw

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-d = document;
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-var cart = {};
-var allListOfCart = {};
 
 
 const app = new Vue({
@@ -9,40 +6,69 @@ const app = new Vue({
     data: {
         goods: [],
         filteredGoods: [],
-        searchLine: ''
+        searchLine: '',
+        cart: {}
     },
     methods: {
         filterGoods(value) {
             const regexp = new RegExp(this.searchLine, 'i');
             this.filteredGoods = this.goods.filter(good => regexp.test(good.product_name));
-            /*this.addSettings();*/
         },
 
         makeGETRequest(url, callback) {
-            const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-
             var xhr;
-
             if (window.XMLHttpRequest) {
                 xhr = new XMLHttpRequest();
             } else if (window.ActiveXObject) { 
                 xhr = new ActiveXObject("Microsoft.XMLHTTP");
             }
-
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4) {
                     callback(JSON.parse(xhr.responseText));
                 }
             }
-
             xhr.open('GET', url, true);
             xhr.send();    
         },
+
+        renderCart(){
+
+        },
+
+        minus(id){
+            this.cart[id].count--;
+            if (this.cart[id].count < 1) {
+                delete this.cart[id] 
+            }
+            console.log(this.cart[id])
+            localStorage.setItem('cart', JSON.stringify(this.cart));
+        },
+        
+        addToCart(id, name, price){
+            if (this.cart[id] == undefined) {
+                let x = {
+                    id_product: id,
+                    product_name: name,
+                    price: price,
+                    count: 1
+                }
+                this.cart[id] = x;
+            }   else    {
+                this.cart[id].count = 1 + this.cart[id].count;   
+            }                 
+            localStorage.setItem('cart', JSON.stringify(this.cart));
+            console.log(this.cart)
+            
+        }
+
     },
         mounted() {
             this.makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
                 this.goods = goods;
-                this.filteredGoods = goods;         
+                this.filteredGoods = goods;
+                if (localStorage.getItem('cart') != null) {
+                    this.cart = JSON.parse(localStorage.getItem('cart'));
+                }        
             });
         },
 });
